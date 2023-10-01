6e6af316aec02d02d9dc217a59df2dbe4bd4c403
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-01 09:12:40 +0200

    computed property

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,27 @@
 <template>
   <div class="home">
-    <p>{{ ninjaOne.age }} -- {{ ninjaOne.name }}</p>
-    <p>{{ ninjaTwo.age }} -- {{ ninjaTwo.name }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <input type="text" v-model="search">
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
 
 export default {
   name: 'Home',
   setup(){
-    const ninjaOne = ref({name: 'mario', age: 30})
-    const ninjaTwo = reactive({name: 'luigi', age: 35})
+    const search = ref('')
+    const names = ref(['mario', 'luigi','yoshi','peach'])
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45
-    }
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
 
-    return {ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo}
+    return {names, search, matchingNames}
   }
   
 }
