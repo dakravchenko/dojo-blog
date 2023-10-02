987157d2aca5000ca3800fa435a1226ac4b9c8af
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-02 19:53:04 +0200

    composable function + router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/Home.vue'
+import Home from '../views/Home.vue'
+import Details from '../views/Details.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
+  {
+    path: '/posts/:id',
+    name: 'details',
+    component: Details,
+    props: true
+  }
   
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,27 +5,13 @@
 
 <script>
 
-import {ref} from 'vue'
 import PostList from '@/components/PostList.vue'
+import getPosts from '../assets/composables/getPosts'
 export default {
   name : 'Home',
   components: {PostList},
   setup(){
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok){
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-
-      } catch (error) {
-        error.value = error.message
-      }
-    }
+    const {posts, error, load} = getPosts()
     load()
     return {posts, error}
   }
